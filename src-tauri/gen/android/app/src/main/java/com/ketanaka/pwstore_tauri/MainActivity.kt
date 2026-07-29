bf8041b52a436f086e3792217ce0d0858cf7232a
Ken Tanaka <tanaka-ken@netlab.jp>
2026-07-29 17:39:13 +0900

    Android用のビルド設定ファイルの追加

---
+++ src-tauri/gen/android/app/src/main/java/com/ketanaka/pwstore_tauri/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ketanaka.pwstore_tauri
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
